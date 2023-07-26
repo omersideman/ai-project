329560322fca3aa23ed7aaf848f87d4f4a7c58d6
omersideman <osideman@gmail.com>
2023-07-26 19:44:26 +0300

    added retry loop for api calls

--- tuneprophet/spotify.py
+++ tuneprophet/spotify.py
@@ -3,12 +3,15 @@
 
 import json
 from os import environ
+import time
 from dotenv import load_dotenv
 import spotipy
 import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
 from tuneprophet.utils import pretty_print
 from utils import create_dirs_if_not_exist
+# from Nice that I was treating the data,.notebook import tqdm
+from requests.exceptions import ReadTimeout  # type: ignore
 
 load_dotenv()
 
@@ -21,9 +24,21 @@ class Spotify:
     def __init__(self):
         pass
 
+    def _call_spotify_api(self, func, *args, **kwargs):
+        '''wrapper for spotify api calls to handle timeouts'''
+        for i in range(3):
+            try:
+                return func(*args, **kwargs)
+            except ReadTimeout:
+                print('ReadTimeout - waiting 10 seconds')
+                time.sleep(10)
+                return func(*args, **kwargs)
+
     def get_playlist_info(self, playlist_url: str):
-        # this is stupid but it's not working otherwise
-        info = sp.playlist(playlist_url)
+        ''' returns basic info about a playlist'''
+
+        # should use fields param to make less api calls but not working
+        info = self._call_spotify_api(sp.playlist, playlist_url)
 
         if not info:
             raise ValueError('No results found')
@@ -33,7 +48,8 @@ class Spotify:
         return res
 
     def get_playlist_tracks(self, playlist_url: str):
-        results = sp.playlist_items(playlist_url)
+        '''returns all tracks in a playlist'''
+        results = self._call_spotify_api(sp.playlist_items, playlist_url)
 
         if not results:
             raise ValueError('No results found')
@@ -43,12 +59,18 @@ class Spotify:
         while results['next']:  # type: ignore
             results = sp.next(results)
             tracks.extend(results['items'])  # type: ignore
-        print(len(tracks))
+
+        print(f'{len(tracks)} tracks in playlist')
+
         return tracks
 
     def playlist_to_df(self, playlist_url: str,):
+        '''returns a dataframe of all tracks in a playlist with only relevant info'''
 
-        results = self.get_playlist_tracks(playlist_url)
+        print(f'Getting playlist tracks from spotify for {playlist_url}')
+
+        results = self._call_spotify_api(
+            self.get_playlist_tracks, playlist_url)
 
         if not results:
             raise ValueError('No results found')
@@ -57,11 +79,14 @@ class Spotify:
 
         clean = []
 
-        for track in tracks:
-            artist_uri = track['artists'][0]['uri']
-            artist_info = sp.artist(artist_uri)
-            audio_features = sp.audio_features(track['uri'])[0] # type: ignore
+        print('Creating dataframe')
 
+        for i, track in enumerate(tracks):
+            print(f'Track {i}')
+            artist_uri = track['artists'][0]['uri']
+            artist_info = self._call_spotify_api(sp.artist, artist_uri)
+            audio_features = self._call_spotify_api(
+                sp.audio_features, track['uri'])[0]  # type: ignore
             record = {'track_name': track['name'],
                       'track_pop': track['popularity'],
                       'artist': track['artists'][0]['name'],
@@ -78,12 +103,13 @@ class Spotify:
         return df
 
     def combine_to_csv(self, playlist_urls: list, name: str):
+        '''end to end function to get tracks from multiple playlists and save to csv'''
 
         dataframes = [self.playlist_to_df(url) for url in playlist_urls]
         combined_df = pd.concat(dataframes, ignore_index=True)
         unique_df = combined_df.drop_duplicates()
 
-        print(len(combined_df))
+        print(f'{len(combined_df)} tracks in total')
 
         filepath = f'../data/{name}.csv'
 
