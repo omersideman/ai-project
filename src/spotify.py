dd424c4ddb99dddad20a824a4df2d63d6e038a31
omersideman <osideman@gmail.com>
2023-08-02 16:36:25 +0300

    basic data analysis

--- src/spotify.py
+++ src/spotify.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 import spotipy
 import pandas as pd
 from spotipy.oauth2 import SpotifyClientCredentials
-from utils import create_dirs_if_not_exist
+from file_utils import create_dirs_if_not_exist
 from requests.exceptions import ReadTimeout  # type: ignore
 
 load_dotenv()
