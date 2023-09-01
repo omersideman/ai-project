42a02ee39b7ed826097589adf4f65b6dbac08bd2
TomerSt123 <55196588+TomerSt123@users.noreply.github.com>
2023-09-01 21:18:40 +0300

    critical fix in the plotCV function

--- project/cross_val.py
+++ project/cross_val.py
@@ -27,8 +27,6 @@ def setConfigure(dict_configure):
 
 def crossValidate(model, X, y, kf):
     first = True
-    #list_apply = lambda l1,l2,l3,l4,func: (func(l1,l3),func(l2,l4)) 
-    #list_sum = lambda l1, l2: [x1+x2 for x1,x2 in zip(l1,l2)]
     folds = kf.get_n_splits()
     for (train_index, test_index) in kf.split(X):
         train_fold_res, test_fold_res = model.fit(X[train_index,:],y[train_index], X[test_index,:], y[test_index])
@@ -39,9 +37,5 @@ def crossValidate(model, X, y, kf):
         else:
             res_train += torch.tensor(train_fold_res)
             res_test += torch.tensor(test_fold_res)
-    #        res_train = list_apply(*res_train, *train_fold_res, list_sum)
-    #        res_test = list_apply(*res_test, *test_fold_res, list_sum)
-
-    #div = lambda l,d: [x/d for x in l]
-    #return list_apply(*res_train, folds,folds, div), list_apply(*res_test, folds,folds, div)
+    
     return (res_train/folds).tolist(), (res_test/folds).tolist()
