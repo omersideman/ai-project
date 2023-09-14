5fa6ddd6c478f10cfca8f691eae6f5cbe9f7a255
TomerSt123 <55196588+TomerSt123@users.noreply.github.com>
2023-09-14 20:28:26 +0300

    continue with RNN

---
+++ src/RNN_utils/dataset.py
@@ -0,0 +1,35 @@
+from torch.utils.data import Dataset
+from src.RNN_utils.audio_utils import rechannel, get_chorus, createSpect
+import torchaudio
+
+
+class SoundDS(Dataset):
+	def __init__(self, df, data_path, channel = 1, ext = '.mp3', duration = 30, n_mel=128, n_fft=400,win_length=400):
+		self.df = df
+		self.data_path = str(data_path)
+		self.channel = channel
+		self.ext = ext
+		self.duration = duration
+		self.n_mel = n_mel
+		self.n_fft = n_fft
+		self.win_length = win_length
+	
+	def __len__(self):
+		return len(self.df)    
+
+	def __getitem__(self, idx):
+		song_path = self.data_path + '/' + self.df.loc[idx,'id'] + self.ext
+		class_id = self.df.loc[idx, 'viral']
+
+		#load the audio file
+		aud = torchaudio.load(song_path)
+
+		#convert the audio to mono audio
+		aud = rechannel(aud,new_channel=1)
+
+		#take only the part of the chorus from the signal
+		aud = get_chorus(song_path, self.duration, aud)
+
+		#create the mel-spectogram
+		sgram = createSpect(aud, n_mels=self.n_mel, n_fft=self.n_fft, win_length=self.win_length)
+		return sgram, class_id
--- src/main.py
+++
@@ -1,5 +0,0 @@
-from tests.iterator_test import dataIteratorTest
-
-
-if __name__ == "__main__":
-    dataIteratorTest()
