6bcfe9f1d4a656d471a8f45d1aa47c9445ec8f43
omersideman <osideman@gmail.com>
2023-09-16 18:39:30 +0300

    trying to fix training error

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -1,39 +1,71 @@
 import os
 import pandas as pd
-from file_utils import create_dirs_if_not_exist
+from shutil import copyfile
 
 
-def split_classes(csv_path, spectrograms_path, out_path, class_names, high_th, low_th):
-    class1_path = os.path.join(out_path, class_names[0]) + '/'
-    class2_path = os.path.join(out_path, class_names[1]) + '/'
+def split_classes_threshold(csv_path, spectrograms_path, out_path, class_names, high_th, low_th):
+    class1_path, class2_path = [os.path.join(
+        out_path, name) for name in class_names]
 
-    # create directories
-    create_dirs_if_not_exist(class1_path)
-    create_dirs_if_not_exist(class2_path)
+    # Create directories if they don't exist
+    for class_path in [class1_path, class2_path]:
+        os.makedirs(class_path, exist_ok=True)
 
     df = pd.read_csv(csv_path)
-    # iterate through rows
-    for index, row in df.iterrows():
-        # get popularity
-        popularity = row['number_of_videos']
-        # get spectrogram path
-        spectrogram_path = os.path.join(
-            spectrograms_path, f"{row['id']}.png")
-        # get class
-        if popularity >= high_th:
-            class_path = class1_path
-        elif popularity <= low_th:
-            class_path = class2_path
-        else:  # skip if popularity is in between
-            continue
-        # copy spectrogram to class directory
-        os.system(f"cp {spectrogram_path} {class_path}")
 
-    # print number of files in each class
-    print(
-        f"Number of files in {class_names[0]}: {len(os.listdir(class1_path))}")
-    print(
-        f"Number of files in {class_names[1]}: {len(os.listdir(class2_path))}")
+    # Copy spectrograms to class directories based on popularity
+    for _, row in df.iterrows():
+        popularity = row['number_of_videos']
+
+        spectrogram_path = os.path.join(spectrograms_path, f"{row['id']}.png")
+        class_path = class1_path if popularity >= high_th else (
+            class2_path if popularity <= low_th else None)
+        print(f"popularity: {popularity}, class_path: {class_path}")
+        if class_path and os.path.exists(spectrogram_path):
+            copyfile(spectrogram_path, os.path.join(
+                class_path, os.path.basename(spectrogram_path)))
+
+    # Print the number of files in each class
+    for class_path, class_name in zip([class1_path, class2_path], class_names):
+        file_count = len(os.listdir(class_path))
+        print(f"Number of files in {class_name}: {file_count}")
+
+
+def split_classes(csv_path, spectrograms_path, out_path, class_names, num_tracks):
+    '''splits into directories of the first and last num_tracks tracks'''
+
+    class1_path, class2_path = [os.path.join(
+        out_path, name) for name in class_names]
+
+    # Create directories if they don't exist
+    for class_path in [class1_path, class2_path]:
+        os.makedirs(class_path, exist_ok=True)
+
+    df = pd.read_csv(csv_path)
+
+    # get ids of first num_tracks and last num_tracks
+    first_ids = df['id'][:num_tracks]
+    last_ids = df['id'][-num_tracks:]
+
+    first_png_paths = [os.path.join(
+        spectrograms_path, f"{id}.png") for id in first_ids]
+    last_png_paths = [os.path.join(
+        spectrograms_path, f"{id}.png") for id in last_ids]
+
+    for png_path in first_png_paths:
+        if os.path.exists(png_path):
+            copyfile(png_path, os.path.join(
+                class1_path, os.path.basename(png_path)))
+
+    for png_path in last_png_paths:
+        if os.path.exists(png_path):
+            copyfile(png_path, os.path.join(
+                class2_path, os.path.basename(png_path)))
+
+    # Print the number of files in each class
+    for class_path, class_name in zip([class1_path, class2_path], class_names):
+        file_count = len(os.listdir(class_path))
+        print(f"Number of files in {class_name}: {file_count}")
 
 
 def how_many(csv_path, column_name, value, where='above'):
