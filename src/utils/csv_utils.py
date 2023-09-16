b839b26951892bb157b0cc0041bd6a84c19ed671
omersideman <osideman@gmail.com>
2023-09-16 12:42:12 +0300

    notebook works in google collab

--- src/utils/csv_utils.py
+++ src/utils/csv_utils.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
-from .file_utils import create_dirs_if_not_exist
+from file_utils import create_dirs_if_not_exist
+
 
 def split_classes(csv_path, spectrograms_path, out_path, class_names, high_th, low_th):
     class1_path = os.path.join(out_path, class_names[0]) + '/'
@@ -23,11 +24,23 @@ def split_classes(csv_path, spectrograms_path, out_path, class_names, high_th, l
             class_path = class1_path
         elif popularity <= low_th:
             class_path = class2_path
-        else: # skip if popularity is in between
+        else:  # skip if popularity is in between
             continue
         # copy spectrogram to class directory
         os.system(f"cp {spectrogram_path} {class_path}")
 
     # print number of files in each class
-    print(f"Number of files in {class_names[0]}: {len(os.listdir(class1_path))}")
-    print(f"Number of files in {class_names[1]}: {len(os.listdir(class2_path))}")
+    print(
+        f"Number of files in {class_names[0]}: {len(os.listdir(class1_path))}")
+    print(
+        f"Number of files in {class_names[1]}: {len(os.listdir(class2_path))}")
+
+
+def how_many(csv_path, column_name, value, where='above'):
+    df = pd.read_csv(csv_path)
+    if where == 'above':
+        return len(df[df[column_name] >= value])
+    elif where == 'below':
+        return len(df[df[column_name] <= value])
+    else:
+        raise ValueError(f"Invalid value for where: {where}")
